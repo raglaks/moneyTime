b6830336048d4e8027704944fc61d3cc6e712571
Edgar Burelo <edgar.burelo@gmail.com>
2018-11-12 22:28:20 -0500

    expenses Model

---
+++ models/expenses.js
@@ -0,0 +1,35 @@
+module.exports = function (sequelize, DataTypes) {
+    const expenses = sequelize.define("expenses", {
+        userid: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        expName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [0, 50],
+                    msg: 'The name is too long'
+                }
+            }
+        },
+        finAccount: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        expType: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        expDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        expAmmount: {
+            type: DataTypes.FLOAT(11, 2),
+            allowNull: false
+        }
+    });
+    return expenses;
+};
--- models/usersM.js
+++ models/usersM.js
@@ -1,4 +1,4 @@
-module.exports = function(sequelize, DataTypes) {
+ module.exports = function(sequelize, DataTypes) {
     const users = sequelize.define("users", {
         id: {
             type: DataTypes.INTEGER,
