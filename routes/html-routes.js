ec52f3280a2a040bfb0cd2eadf08567d2a0c7036
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-14 17:53:15 -0600

    added template for new entry into DB

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,10 +2,24 @@
 
 module.exports = function (app) {
 
+    //route for index (home page)
     app.get("/", function (req, res) {
 
-        res.render("../layouts/index.handlebars");
+        res.render("index");
 
     });
 
+    //route for user account overview 
+    app.get("/account", function (req, res) {
+
+        res.render("account");
+
+    });
+
+    //route for new entry into DB
+    app.get("/new_entry", function (req, res) {
+
+        res.render("new_entry");
+
+    });
 }
