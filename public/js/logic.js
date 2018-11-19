df58131f34a83a09ea7ae54c55b98bba9abb2bd7
Edgar Burelo <edgar.burelo@gmail.com>
2018-11-19 12:51:24 -0600

    insert data finAccount

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
-    console.log("hello World");
+    //console.log("hello World");
 
    
 
     $("#inputGroupSelect01").change(() => {
-        console.log($("#inputGroupSelect01").val());
+        //console.log($("#inputGroupSelect01").val());
         let accType = $("#inputGroupSelect01").val();
+        let target1 = $("#statementDate");
+        let target2 = $("#dueDate");
+        //Target clean
+        target1.empty();
+        target2.empty();
+
         if (accType == "credit") {
             //target to append the divs
-            let target1 = $("#statementDate");
-            let target2 = $("#dueDate");
+            
             //div creation
             let statementDiv = $('<div class="input-group-prepend">');
             let statementSpan = $('<span class="input-group-text" id="inputGroup-sizing-default">');
             statementSpan.text("Statement Date");
-            let statementInput = $('<input class="calendar form-control" type="text" placeholder="Select Date.." readonly="readonly">');
+            let statementInput = $('<input id="statDate" class="calendar form-control" type="text" placeholder="Select Date.." readonly="readonly">');
             
             //apending for target 1
             target1.append(statementDiv);
@@ -25,7 +30,7 @@ $(document).ready(function() {
             let dueDiv = $('<div class="input-group-prepend">');
             let dueSpan = $('<span class="input-group-text" id="inputGroup-sizing-default">');
             dueSpan.text("Due Date");
-            let dueInput = $('<input class="calendar form-control" type="text" placeholder="Select Date.." readonly="readonly">');
+            let dueInput = $('<input id="dueD" class="calendar form-control" type="text" placeholder="Select Date.." readonly="readonly">');
             target2.append(dueDiv);
             dueDiv.append(dueSpan);
             target2.append(dueInput);
@@ -41,12 +46,12 @@ $(document).ready(function() {
         event.preventDefault();
         const accName = $("#accNmae").val();
         const accType = $("#inputGroupSelect01").val();
-        let statementDate = "";
-        let dueDate = "";
+        let statementDate = false;
+        let dueDate = false;
 
         if(accType == "credit") {
             statementDate = $("#statDate").val();
-            dueDate = $("#dueDate").val();
+            dueDate = $("#dueD").val();
         }
         
         //sended Object
@@ -58,9 +63,13 @@ $(document).ready(function() {
         };
         
         console.log(data);
+        let path = $(location).attr('pathname');
+        let pathArr = path.split("/");
+        let id = pathArr[3];
+        console.log(id);
 
         //FALTA LA ROUTE CORRECTA!!!!!!!
-        $.ajax("/", {
+        $.ajax("/api/accounts/"+id, {
             type: "POST",
             data
         }).then(() => {
