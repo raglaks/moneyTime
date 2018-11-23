5015df5090a4f65f8328fca9244dd1f7f381abcf
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-23 16:03:01 -0600

    add deploy details for heroku and jawsdb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
